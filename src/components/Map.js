0d71756798077aa4f157632422678a3943a1f4c3
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-08-31 15:53:13 -0500

    map added

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,24 +1,36 @@
-import React, {useState} from 'react';
-import ReactMapGL from "react-map-gl";
+import React, { useRef, useEffect, useState } from 'react';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import mapboxgl from '!mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoibm9zYWlsZSIsImEiOiJjbDB0emc1ZHAwMW5xM2luemd6aHk2YXJzIn0.5tPAhVIwS7bSeTG0BZOaow';
+
 
 
 function Map() {
 
 
-    const [viewPort, setViewport] = useState({
-        latitude: 45.42,
-        longitude: -75.69,
-        width: "750px",
-        height: "400px",
-        zoom: 8
-    })
+    const mapContainer = useRef(null);
+const map = useRef(null);
+const [lng, setLng] = useState(-82.2800);
+const [lat, setLat] = useState(35.8278);
+const [zoom, setZoom] = useState(9);
+
+useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+    container: mapContainer.current,
+    style: 'mapbox://styles/mapbox/outdoors-v11',
+    center: [lng, lat],
+    zoom: zoom
+    });
+    });
+
+
     return (
-        <div>
-            <ReactMapGL {...viewPort} mapboxAccessToken="pk.eyJ1Ijoibm9zYWlsZSIsImEiOiJjbDB0emc1ZHAwMW5xM2luemd6aHk2YXJzIn0.5tPAhVIwS7bSeTG0BZOaow" onViewportChange={setViewport} />
-                
-            
-        </div>
+        
+            <div ref={mapContainer} className="map-container" />
+        
     )
 }
 
